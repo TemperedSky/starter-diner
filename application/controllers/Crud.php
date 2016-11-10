d2d355fa40b7fc20e00cfa9261c8a403e865c577
Jason Burns <admiralmudkip@gmail.com>
2016-11-10 15:18:49 -0800

    combo field styled

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -7,9 +7,8 @@ class Crud extends Application
 	function __construct() {
 		parent::__construct();
 	}
-        
     
-    function edit($id=null) {    // try the session first
+    function edit($id=null) {    // try the session first        
         $key = $this->session->userdata('key');
         $record = $this->session->userdata('record');    // if not there, get them from the database
         
@@ -18,19 +17,23 @@ class Crud extends Application
             $key = $id;
             $this->session->set_userdata('key',$id);  
             $this->session->set_userdata('record',$record);
-        } else {
-            // build the form fields
-            $this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
-            $this->data['fname'] = makeTextField('Item name', 'name', $record->name);
-            $this->data['fdescription'] = makeTextField('Description', 'description', $record->description);
-            $this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
-            $this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
-            $this->data['fcategory'] = makeTextField('Category', 'category', $record->category);
-            // show the editing form
-            $this->data['pagebody'] = "mtce-edit";
         }
         
-        $this->data['content'] = "Looking at " . $key . ': ' . $record->name;
+        // build the form fields
+        $this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
+        $this->data['fname'] = makeTextField('Item name', 'name', $record->name);
+        $this->data['fdescription'] = makeTextArea('Description', 'description', $record->description);
+        $this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
+        $this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
+        
+        $categories = $this->categories->all();
+        foreach($categories as $code => $category){
+            $codes[$code] = $category->name;
+        }
+        $this->data['fcategory'] = makeComboBox('Category', 'category', $record->category, $codes);
+        // show the editing form
+        $this->data['pagebody'] = "mtce-edit";
+        
         $this->render();
     }
 }
